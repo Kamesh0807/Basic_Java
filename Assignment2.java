1f853a7ae7390ff7d5b729f095bb9dba6c3f86f3
Kamesh <kameshnfb@gmail.com>
2025-04-28 08:05:11 +0530

    All

---
+++ Assignment2.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class Assignment2
+{
+	static void add()
+	{
+		System.out.println("test");
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Kamesh");
+		add();
+	}
+
+}
---
+++ Assignment3.java
@@ -0,0 +1,30 @@
+package Assignments;
+
+public class Assignment3 {
+
+	public static void main(String[] args) 
+	{
+		int a=100;
+		byte b=10;
+		short c=1;
+		String name=" vk";
+		boolean d=false;
+		double f=0.10d;
+		float g=0.001f;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		System.out.println(c);
+		
+		System.out.println(d);
+		
+		System.out.println(f);
+		System.out.println(g);
+		
+
+		
+
+	}
+
+}
