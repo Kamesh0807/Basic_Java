201e8c6500e95ccbacc1659a5bf466ec15c28be9
Kamesh <kameshnfb@gmail.com>
2025-04-28 08:04:02 +0530

    Second

---
+++ Assignment1.java
@@ -0,0 +1,11 @@
+package Assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) 
+	{
+	
+		String str = null;
+        System.out.println(str.length()); // Throws NullPointerException
+
+	}}
